841d16e571fcd4bb2c394f20a056d3e768532460
RachelLindquist <firefly7491@gmail.com>
2022-06-12 16:41:42 -0400

    Lab 5 part 3 done

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,30 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        //(This task uses Strings:) A program that prompts the user for their party affiliation
+        // (Democrat, Republican, or Independent) and responds accordingly with a Donkey,
+        // Elephant, Person, or Other.  (i.e. “You get a Democratic Donkey.”)
+        // Notes: create a menu so the user chooses D, R, or I and assume that
+        // any other choice will be Other.  Tests: just the four options D, R, I, Other.
+        // Use as cascaded if structure not separate if statements!
+
+        //Test runs:   (insert the output widow copies here for the 4 test runs)
+
+        System.out.println ("Please enter your party affiliation. (D, R, I or Other)");
+        String party = "D";
+        if (party.equals("D")){
+            System.out.println("You get a Democratic Donkey.");
+        } else if (party.equals("R")){
+            System.out.println("You get a Republican Elephant.");
+        } else if (party.equals("I")){
+            System.out.println("You get an Independent Person.");
+        } else {
+            System.out.println("You picked Other, no animal for you.");
+        }
+
+
+
+
+
     }
 }
